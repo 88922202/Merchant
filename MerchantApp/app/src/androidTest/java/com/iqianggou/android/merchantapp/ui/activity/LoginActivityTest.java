ad0f7d5937ae548f030a81c37f42112ae034d8bf
88922202 <88922202@qq.com>
2016-10-05 23:48:00 +0800

    添加界面测试

---
+++ MerchantApp/app/src/androidTest/java/com/iqianggou/android/merchantapp/ui/activity/LoginActivityTest.java
@@ -0,0 +1,60 @@
+package com.iqianggou.android.merchantapp.ui.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.iqianggou.android.merchantapp.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void loginActivityTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.et_username),
+                        withParent(allOf(withId(R.id.activity_login),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.et_username),
+                        withParent(allOf(withId(R.id.activity_login),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("zgzd"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.et_password),
+                        withParent(allOf(withId(R.id.activity_login),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("150130"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_login), withText("login"),
+                        withParent(allOf(withId(R.id.activity_login),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+    }
+
+}
